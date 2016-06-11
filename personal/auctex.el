c7f6cf3f63fd544184709c7bfaf56d642a331519
mbonora <mbonora@pcitsmbonora.dyndns.cern.ch>
2016-06-11 12:05:07 +0200

    Add auctex.el

---
+++ personal/auctex.el
@@ -0,0 +1,22 @@
+;; LaTeX configuration
+(setq TeX-auto-save t)
+(setq TeX-parse-self t)
+(setq-default TeX-master nil)
+
+(add-hook 'LaTeX-mode-hook 'visual-line-mode)
+(add-hook 'LaTeX-mode-hook 'flyspell-mode)
+(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+(add-hook 'LaTeX-mode-hook 'TeX-source-correlate-mode)
+
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq reftex-plug-into-AUCTeX t)
+(setq TeX-PDF-mode t)
+
+(setq TeX-output-view-style
+      (quote
+       (("^pdf$" "." "evince -f %o")
+        ("^html?$" "." "iceweasel %o"))))
+
+;; Setting up writegood-mode
+(require 'writegood-mode)
+(global-set-key "\C-cg" 'writegood-mode)
