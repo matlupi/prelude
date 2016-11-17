b7badbe5f9df40d9dc9fb0bd17869175d13429d9
Matteo Lupi <matteo.lupi@cern.ch>
2016-11-17 18:41:28 +0100

    ML set tex custom engine

--- personal/custom.el
+++ personal/custom.el
@@ -3,17 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-engine (quote luatex))
  '(custom-safe-themes
    (quote
     ("aa87469691932ff791f966bffb885ecd97ebfa4dc4d42e479f3819ac4a3fbcaf" "75c0b9f9f90d95ac03f8647c75a91ec68437c12ff598e2abb22418cd4b255af0" "5999e12c8070b9090a2a1bbcd02ec28906e150bb2cdce5ace4f965c76cf30476" "6df30cfb75df80e5808ac1557d5cc728746c8dbc9bc726de35b15180fa6e0ad9" "c4465c56ee0cac519dd6ab6249c7fd5bb2c7f7f78ba2875d28a50d3c20a59473" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(flycheck-gcc-language-standard "c++11")
  '(global-linum-mode t)
  '(prelude-theme (quote tango-dark))
- '(user-mail-address "matteo.lupi@cern.ch")
  '(show-paren-mode t)
- ;;'(verilog-auto-newline nil)
+ '(user-mail-address "matteo.lupi@cern.ch")
+ '(vhdl-clock-edge-condition (quote function))
+ '(vhdl-clock-name "")
  '(vhdl-company-name "CERN European Organization for Nuclear Research")
- '(vhdl-file-header "-------------------------------------------------------------------------------
+ '(vhdl-copyright-string
+   "-------------------------------------------------------------------------------
+-- Copyright (c) <year>
+")
+ '(vhdl-electric-mode t)
+ '(vhdl-file-header
+   "-------------------------------------------------------------------------------
 -- Title      : <title string>
 -- Project    : <project>
 -------------------------------------------------------------------------------
@@ -36,18 +44,6 @@
 
 <cursor>
 ")
- '(vhdl-clock-name "")
- '(vhdl-clock-edge-condition (quote function))
- '(vhdl-electric-mode t)
- '(vhdl-reset-active-high t)
- '(vhdl-reset-kind (quote sync))
- '(vhdl-reset-name "RST")
- ;;)
- '(vhdl-copyright-string
-   "-------------------------------------------------------------------------------
--- Copyright (c) <year>
-")
- '(vhdl-electric-mode t)
  '(vhdl-platform-spec "CERN OS7, Xilinx Vivado 2015.4")
  '(vhdl-reset-active-high t)
  '(vhdl-reset-kind (quote sync))
