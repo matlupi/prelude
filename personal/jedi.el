a89be5747145910b6d02cadbee4af5544284d4b4
Matteo Lupi <matteo.lupi@cern.ch>
2018-07-18 14:27:55 +0200

    ML updated documentation on jedi for python

--- personal/jedi.el
+++ personal/jedi.el
@@ -1,7 +1,10 @@
+;; Installs Jedi (http://tkf.github.io/emacs-jedi/latest/)
+
 (prelude-require-packages
  '(jedi)
  )
 
-
+;; configures jedi
 (add-hook 'python-mode-hook 'jedi:setup)
-;;(setq jedi:complete-on-dot t)                 ; optional
+;; condigured jedi to autocomplete on .
+(setq jedi:complete-on-dot t)                 ; optional
