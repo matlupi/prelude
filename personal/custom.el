5d1da1b7b75d2603eaabc9c7b643992e0898fa8a
Matteo Lupi <matteo.lupi@cern.ch>
2018-08-07 09:44:22 +0200

    ML added *.ino in c mode

--- personal/custom.el
+++ personal/custom.el
@@ -72,8 +72,8 @@
 
 
 (add-to-list 'auto-mode-alist '("\\.xdc\\'". tcl-mode))
+(add-to-list 'auto-mode-alist '("\\.ino\\'". c-mode))
 (add-to-list 'auto-mode-alist '("\\.el\\'". lisp-mode))
-;;(add-to-list 'auto-mode-alist '("\\.pkg\\'". vhdl-mode))
 
 (require 'package) ;; You might already have this line
 (add-to-list 'package-archives
