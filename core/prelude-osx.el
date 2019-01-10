6e3ec1de8f21c631543a8b4731e78e6f3fd8e3e6
Matteo Lupi <matteo.lupi@cern.ch>
2019-01-10 09:23:52 +0100

    ML removed vkill completely for mac

--- core/prelude-osx.el
+++ core/prelude-osx.el
@@ -44,8 +44,8 @@
 (setq ns-function-modifier 'hyper)
 
 ;; proced-mode doesn't work on OS X so we use vkill instead
-(autoload 'vkill "vkill" nil t)
-(global-set-key (kbd "C-x p") 'vkill)
+;;(autoload 'vkill "vkill" nil t)
+;;(global-set-key (kbd "C-x p") 'vkill)
 
 (defun prelude-swap-meta-and-super ()
   "Swap the mapping of Meta and Super.
