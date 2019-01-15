d67531b338cb73ef09870b7a9fd3932735b0aa8c
Matteo Lupi <matteo.lupi@cern.ch>
2019-01-15 08:31:18 +0100

    ML updated line highlight color to green to be able to see yellow stuff

--- personal/custom.el
+++ personal/custom.el
@@ -11,7 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-line ((t (:background "SeaGreen4")))))
 
 
 (add-to-list 'auto-mode-alist '("\\.xdc\\'". tcl-mode))
